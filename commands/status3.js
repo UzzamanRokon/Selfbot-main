2ba7a4fdf375c50cc6a7b9d53102d59f7902dfd6
UzzamanRokon <uzzaman.8444@gmail.com>
2024-10-12 02:10:37 +0600

    Update status3.js

--- commands/status3.js
+++ commands/status3.js
@@ -17,17 +17,17 @@ module.exports = {
                 {
                     name: 'Self Respect',
                     details: 'Self respect',
-                    largeImageURL: 'https://cdn.discordapp.com/attachments/1218882306475950204/1233074089778876467/pngimg.com_-_number1_PNG14889.png?ex=662bc53c&is=662a73bc&hm=fafb310a8226b5afe86286ae5959e3bb23287f9291793973c969c46296416d85&'
+                    largeImageURL: 'https://cdn.discordapp.com/attachments/1293319346487169054/1294332809133031466/The-Dark-Side-of-Self-Improvement.png?ex=670aa0dd&is=67094f5d&hm=a0df1d426a56c0c49b8100a83522df6d73fb5fbdbd2e2cd8639cf6fb4da3fe2e&'
                 },
                 {
-                    name: 'Self Improvement',
-                    details: 'Self improvement',
-                    largeImageURL: 'https://cdn.discordapp.com/attachments/1218882306475950204/1233074090244440094/number-2-tm-photo-16.png?ex=662bc53c&is=662a73bc&hm=931e899b927203b1cf52080c872de42217ddd1543b8e051322b0db09fa16d74f&'
+                    name: 'Autistic',
+                    details: 'autistxc',
+                    largeImageURL: 'https://media.discordapp.net/attachments/1284783750324031499/1294354229934424085/eb2efa7eddbb52b1132cdcd153cc86e0.png?ex=670ab4d0&is=67096350&hm=645d3a62a1ba8ff4be589c04b0398b404ad20db47e4218a4ef1c8492f2464e47&'
                 },
                 {
-                    name: 'Proper Diet',
-                    details: 'Proper diet',
-                    largeImageURL: 'https://cdn.discordapp.com/attachments/1218882306475950204/1233074090647224340/images_1.png?ex=662bc53c&is=662a73bc&hm=c4d084292296c819f07b9151af8533490849086838032573f7ac7c2f5dff1a15&'
+                    name: 'Broken Heart',
+                    details: 'My Heart Broken by my Love ',
+                    largeImageURL: 'https://media.discordapp.net/attachments/1284783750324031499/1294354054150885387/badmash-boy-alone-boy-badmash-boy-broken-heart-broken-love-sad_968529-95325.png?ex=670ab4a6&is=67096326&hm=51fa6f65dd076ed230f19ff5132eb9329956f3fbd345a51701271324051c4fad&'
                 }
             ];
 
@@ -41,9 +41,9 @@ module.exports = {
                 .setName(status.name)
                 .setDetails(status.details)
                 .setAssetsLargeImage(status.largeImageURL) // Change to status.largeImageURL
-                .setAssetsLargeText('devrock.exe') // Set as needed
-                .addButton('Youtube', 'https://www.youtube.com/@ZenithSenpai') // Add button name and URL
-                .addButton('Discord', 'https://discord.gg/Rcn5ZhPt9J'); // Add button name and URL
+                .setAssetsLargeText('autistxc') // Set as needed
+                .addButton('Youtube', 'https://www.youtube.com/@RokonUzzaman-kz3pb') // Add button name and URL
+                .addButton('Discord', 'https://discord.gg/8DWWpzwXg9'); // Add button name and URL
 
             client.user.setActivity(r);
 
