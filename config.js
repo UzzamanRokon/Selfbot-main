17c01f3860d9b134c6a274ec5d7d0f8d7f301d05
UzzamanRokon <uzzaman.8444@gmail.com>
2024-10-12 02:07:13 +0600

    onfig.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "Your_Token",
-    prefix: "!",
-    allowedUserIDs: ['yourid1', 'yourid2', 'yourid3']
+    token: "MTIxOTI5NDczNjIzMDI1NjY4MA.GjQCPQ.1gQEZk7c-q6Rpki-q0tuOyuRSsIOOCWO9zpfMs",
+    prefix: "",
+    allowedUserIDs: ['nothingid1', '1219294736230256680', 'yourid3']
 };
